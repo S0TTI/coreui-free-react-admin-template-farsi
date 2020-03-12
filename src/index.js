bdf1393213d5efafde74dac7bc53db31edf3aaa6
sotti <sotti0101@gmail.com>
2020-03-12 20:12:33 +0330

    add vazir font

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// import vazir font
+import 'vazir-font/dist/font-face.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
